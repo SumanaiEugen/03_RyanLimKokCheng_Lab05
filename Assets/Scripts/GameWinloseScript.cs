a2e84640d8bc0589c42625bd1b3a625494272d5f
SumanaiEugen <ryanlim943@gmail.com>
2022-01-31 01:08:32 +0800

    Added Restart Script and UI
    Added UI and restart script into win and lose scene

---
+++ Assets/Scripts/GameWinloseScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameWinloseScript : MonoBehaviour
+{
+    public void restart()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+}
